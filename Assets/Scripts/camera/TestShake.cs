4b3e0c1e2744cd2697cf2b9cd541547a2eb41022
dddam103 <dddam103@gmail.com>
2022-10-30 21:53:34 +0900

    camera

---
+++ Assets/Scripts/camera/TestShake.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class TestShake : MonoBehaviour
+{
+    void Update()
+    {
+        // if(Input.GetKeyDown("c"))
+        // {
+        //     ShakeCamera.Instance.Shake(3f,0.1f);
+        // }
+    }
+}
