b45b29cb8b618b35e1a4a69ba08c68895620a0a5
dddam103 <dddam103@gmail.com>
2022-11-07 20:57:25 +0900

    map_test

--- Assets/Scripts/camera/Test.cs
+++ Assets/Scripts/camera/Test.cs
@@ -9,6 +9,7 @@ public class Test : MonoBehaviour
     public CinemachineConfiner cam;    
     public Collider2D Room1;
     public Collider2D Room2;
+    public Collider2D Room3;
     private void Start() {
         cam.GetComponent<CinemachineConfiner>();
     }
@@ -21,8 +22,11 @@ public class Test : MonoBehaviour
         else if(other.CompareTag("Room2"))
         {
             cam.m_BoundingShape2D = Room2;
-        } else
-            cam.m_BoundingShape2D = null;
+        }
+        else if(other.CompareTag("Room3"))
+        {
+            cam.m_BoundingShape2D = Room3;
+        }
     }
     // private void OnTriggerExit2D(Collider2D other)
     // {
