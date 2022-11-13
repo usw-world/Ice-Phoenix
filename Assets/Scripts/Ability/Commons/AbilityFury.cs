11bd04b497db8bda15199df1830b7487961f735a
usoock13 <usoock13@gmail.com>
2022-11-14 02:29:46 +0900

    start ability function.

---
+++ Assets/Scripts/Ability/Commons/AbilityFury.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AbilitySystem;
+
+public class AbilityFury : Ability {
+    [Range(0f, 1f)]
+    float weight = 0f;
+    
+    protected override int maxLevel {
+        get { return 3; }
+    }
+    float[] increasingPercents = {15, 40, 65};
+
+    public override void OnGetAbility() {
+
+    }
+    protected override void Start() {
+        base.Start();
+    }
+}
