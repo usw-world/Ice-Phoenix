321bf9bdae1c925c339948c84e19072f07be5e24
Choi <shwjs001@naver.com>
2022-12-05 16:58:57 +0900

    viking

---
+++ Assets/Scripts/Monster/Viking.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameObjectState;
+
+public class Viking : ChaseMonster {
+    const string ATTACK_STATE_TAG = "tag:Attack";
+
+    State attackState = new State("Attack", ATTACK_STATE_TAG);
+    State hitState = new State("Hit");
+
+    private float attackDamage = 15f;
+    private float lastAttackTime = 0f;
+    private float attackInterval = 1.2f;
+    private float attackDistance = 1.5f;
+
+    [SerializeField] Range attackArea;
+    Range damageArea {
+        get {
+            return new Range(
+                (Vector2)transform.position + attackArea.center * new Vector2(transform.localScale.x, 1),
+                attackArea.bounds
+            );
+        }
+    }
+
+    #region Coroutines
+    // Coroutine setDistanceCoroutine;
+    Coroutine dieCoroutine;
+    Coroutine hitCoroutine;
+    #endregion
+
+    protected override void Awake() {
+        base.Awake();
+
+    }
+    protected override void Start() {
+        base.Start();
+        StartCoroutine(Patrol());
+    }
+    protected override void InitializeState() {
+        base.InitializeState();
+        idleState.OnActive += (nextState) => {
+            monsterAnimator.SetBool("Idle", true);
+        };
+        idleState.OnInactive += (prevState) => {
+            monsterAnimator.SetBool("Idle", false);
+        };
+        chaseState.OnActive += (nextState) => {
+            monsterAnimator.SetBool("Chase", true);
+        };
+        chaseState.OnInactive += (prevState) => {
+            monsterAnimator.SetBool("Chase", false);
+        };
+        attackState.OnActive += (nextState) => {
+            LookAtX(targetDirection.x);
+            monsterAnimator.SetBool("Attack", true);
+        };
+        attackState.OnInactive += (prevState) => {
+            monsterAnimator.SetBool("Attack", false);
+        };
+        hitState.OnActive += (prevState) => {
+            monsterAnimator.SetTrigger("Hit");
+        };
+        dieState.OnActive += (prevState) => {
+            monsterAnimator.SetBool("Die", true);
+        };
+        dieState.OnInactive += (nextState) => {
+            monsterAnimator.SetBool("Die", false);
+        };
+    }
+    protected override void Update() {
+        base.Update();
+        if(isDead) return;
+        if(targetTransform != null) {
+            AttackToTarget();
+        }
+        print(monsterStateMachine.currentState);
+    }
+    void AttackToTarget() {
+        if(lastAttackTime > 0) lastAttackTime -= Time.deltaTime;
+        else {
+            if(remainingDistance <= attackDistance
+            && !monsterStateMachine.Compare(ATTACK_STATE_TAG)
+            && !monsterStateMachine.Compare(hitState)) {
+                monsterStateMachine.ChangeState(attackState);
+            }
+        }
+    }
+    void AnimationEvent_DamageTarget() {
+        Collider2D collider = Physics2D.OverlapBox(damageArea.center, damageArea.bounds, 0, Player.DEFAULT_PLAYER_LAYERMASK);
+        lastAttackTime = attackInterval;
+        if(collider && collider.tag == "Player"
+        && !CheckWallBetween(collider.transform, attackArea.center.y)) {
+            Vector2 force = ((collider.transform.position - transform.position).normalized + Vector3.up*.5f) * 300f;
+            collider.GetComponent<Player>().OnDamage(attackDamage, force);
+        }
+    }
+    public void AnimationEvent_AttackEnd() {
+        monsterStateMachine.ChangeState(idleState);
+    }
+    protected override void OnDrawGizmos() {
+        base.OnDrawGizmos();
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(damageArea.center, damageArea.bounds);
+    }
+
+    public override void OnDamage(float damage, float duration=0) {
+        if(isDead) return;
+        base.OnDamage(damage, duration);
+        if (hp <= 0) {
+            Die();
+        }
+    }
+    public override void OnDamage(float damage, Vector2 force, float duration=0) {
+        if(isDead) return;
+        monsterRigidbody.AddForce(force);
+        if(hitCoroutine != null)
+            StopCoroutine(hitCoroutine);
+        hitCoroutine = StartCoroutine(HitCoroutine(force, duration));
+        OnDamage(damage, duration);
+    }
+    private IEnumerator HitCoroutine(Vector2 force, float duration) {
+        if(duration > 0) {
+            monsterStateMachine.ChangeState(hitState);
+            yield return new WaitForSeconds(duration);
+            monsterStateMachine.ChangeState( isDead ? dieState :  idleState);
+        }
+    }
+
+    protected override void Die() {
+        base.Die();
+        monsterSideUI.gameObject.SetActive(false);
+        monsterStateMachine.ChangeState(dieState);
+    }
+    public void AnimationEvent_DieEnd() {
+        gameObject.SetActive(false);
+    }
+    public IEnumerator Patrol() {
+        while(!isDead) {
+            DetectTarget();
+            yield return new WaitForSeconds(.4f);
+        }
+    }
+    protected override void DetectTarget() {
+        Collider2D inner = Physics2D.OverlapCircle((Vector2)transform.position + detectRange.center, detectRange.radius, Player.DEFAULT_PLAYER_LAYERMASK);
+        if(inner != null) {
+            targetTransform = inner.transform;
+            remainingDistance = Vector2.Distance(new Vector2(transform.position.x, 0), new Vector2(targetTransform.position.x, 0));
+        } else {
+            MissTarget();
+        }
+    }
+    protected override void MissTarget() {
+        targetTransform = null;
+    }
+    protected override bool IsArrive() {
+        return base.IsArrive();
+    }
+    protected override bool CanChase() {
+        return !monsterStateMachine.Compare(ATTACK_STATE_TAG)
+            && !monsterStateMachine.Compare(hitState)
+            && !monsterStateMachine.Compare(dieState)
+            /*&& CheckDirection()*/
+            && base.CanChase();
+    }
+    bool CheckDirection() {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x + targetDirection.x*.7f, transform.position.y), Vector2.down, 1.1f);
+        return !!hit;
+    }
+    private bool CheckWallBetween(Transform target, float pointY) {
+        Collider2D collider;
+        print(pointY);
+        if(target.TryGetComponent<Collider2D>(out collider)) {
+            RaycastHit2D hit = Physics2D.Raycast(
+                (Vector2)transform.position + new Vector2(0, pointY),
+                new Vector2(transform.localScale.x, 0),
+                Vector2.Distance((Vector2)transform.position, target.position) - target.GetComponent<Collider2D>().bounds.size.x/2,
+                1<<6
+            );
+            print((Vector2)transform.position + new Vector2(0, pointY) + " ~ " + ((Vector2)transform.position + new Vector2(0, pointY) + (Vector2.Distance((Vector2)transform.position, target.position) - target.GetComponent<Collider2D>().bounds.size.x/2) * new Vector2(transform.localScale.x, 0)));
+            return !!hit && hit.collider.tag == "Ground";
+        }
+        return false;
+    }
+}
